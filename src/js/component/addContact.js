7af87952986688680e35d3cd11315549c6c1fb69
Guillermo Herrera <128089822+gaha20@users.noreply.github.com>
2023-09-19 19:00:40 +0000

    new contact and card created

---
+++ src/js/component/addContact.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+import "../../styles/demo.css";
+
+const initialValues = {
+    full_name: "",
+    email: "",
+    agenda_slug: "gaha20",
+    address: "",
+    phone: ""
+}
+
+const NewContact = () => {
+    const { store, actions } = useContext(Context);
+    const [contact, setContact] = useState(initialValues);
+    const navigate = useNavigate()
+    const { id } = useParams();
+    console.log(id);
+    const handleContact = (event) => {
+        setContact({ ...contact, [event.target.name]: event.target.value })
+    };
+    const addNewContact = async (event) => {
+        event.preventDefault();
+        if (id == undefined) {
+            const response = await actions.addContacts(contact);
+            if (response) navigate("/")
+        } else {
+            const response = await actions.updateContacts(contact, id)
+            if (response) navigate("/")
+        }
+    }
+
+    return (
+        <div className="container w-100">
+            <form className="d-flex flex-column justify-content-center align-items-center" onSubmit={addNewContact}>
+                <div className="mb-3 col-6">
+                    <label htmlFor="InputFullName" className="form-label">Full Name</label>
+                    <input type="text" className="form-control" id="InputFullName" name="full_name" value={contact.full_name} onChange={(event) => handleContact(event)} />
+                </div>
+                <div className="mb-3 col-6">
+                    <label htmlFor="InputEmail" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" name="email" value={contact.email} onChange={(event) => handleContact(event)} />
+                </div>
+                <div className="mb-3 col-6">
+                    <label htmlFor="PhoneNumber" className="form-label">Phone Number</label>
+                    <input type="text" className="form-control" id="PhoneNumber" name="phone" value={contact.phone} onChange={(event) => handleContact(event)} />
+                </div>
+                <div className="mb-3 col-6">
+                    <label htmlFor="InputAddress" className="form-label">Address</label>
+                    <input type="text" className="form-control" id="InputAddress" name="address" value={contact.address} onChange={(event) => handleContact(event)} />
+                </div>
+                <button type="submit" className="btn btn-primary col-6">Save</button>
+            </form>
+            <Link to="/">back to contacts</Link>
+        </div>
+    );
+};
+
+export default NewContact;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,64 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import rigo from "../../img/rigo-baby.jpg";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	console.log(store.contactList);
+	return (
+	  <>
+		{store.contactList.map((contact) => {
+		  return (
+			<div class="row row-cols-1 row-cols-md-3 g-4">
+				<div key={contact.id} className="col">
+				<div className="card" style={{ width: "18rem" }}>
+					<img
+					className="contact-image"
+					src={rigo}
+					style={{ maxWidth: "300px" }}
+					/>
+					<ul className="list-group list-group-flush">
+					<li className="list-group-item text-center">
+						<h3>{contact.full_name}</h3>
+					</li>
+					<li className="list-group-item">
+						<strong>Phone: </strong>
+						{contact.phone}
+					</li>
+					<li className="list-group-item">
+						<strong>Email: </strong>
+						{contact.email}
+					</li>
+					<li className="list-group-item">
+						<strong>Address: </strong>
+						{contact.address}
+					</li>
+					<li className="list-group-item">
+						<span
+						onClick={() => {
+							actions.removeContacts(contact.id);
+						}}
+						>
+						<Link>
+							<i className="fas fa-trash"></i>
+						</Link>
+						</span>
+						<span>
+						<Link to={`/newcontact/${contact.id}`}>
+							<i className="fa-solid fa-pencil text-dark"></i>
+						</Link>
+						</span>
+					</li>
+					</ul>
+				</div>
+				<br />
+				</div>
+			</div>
+		  );
+		})}
+	  </>
+	);
+  };
